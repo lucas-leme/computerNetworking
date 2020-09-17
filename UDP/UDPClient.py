9444a657c4b3287898baf39837a326131f9fae7a
Lucas Leme <lucaslssantos99@gmail.com>
2020-09-17 16:17:12 -0300

    Modify first UDP message

--- UDP/UDPClient.py
+++ UDP/UDPClient.py
@@ -7,9 +7,9 @@ udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 destiny = (HOST, PORT)
 
-message = bytes('Messagem de teste', 'utf-8')
+message = bytes('Início da comunicação', 'utf-8')
 
-END_VALUE = bytes('end', 'utf-8')
+END_VALUE = bytes('termina', 'utf-8')
 
 while message != END_VALUE:
     udp_client.sendto (message, destiny)
